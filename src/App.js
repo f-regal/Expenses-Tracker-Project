8a96e5959e98b3a5945002a8ea91d1c42382d77e
Faysal Regal <faysalregal@gmail.com>
2021-08-21 23:38:49 +0100

    ChangedAppJsViaMac

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div>
-      <h2>Let's get started! Here we go !!!</h2>
+      <h2>Let's get started! Here we go !!! Changed again fammm!!</h2>
     </div>
   );
 }
