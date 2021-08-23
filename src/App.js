d7eac51ae6df134eaa123cd25de97835ee9823de
f-regal <faysalregal@gmail.com>
2021-08-23 20:37:56 +0100

    AddedComponents
    - added expense item component
    - added expense item styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import ExpenseItem from "./Components/ExpenseItem";
+
 function App() {
   return (
     <div>
       <h2>Let's get started! Here we go !!! Changed again fammm!! Changed on Windows again!! One more time for windows</h2>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
