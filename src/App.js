7edac1b2bad85865d175e16c2d61fccbbe84db5a
f-regal <faysalregal@gmail.com>
2021-08-25 17:22:13 +0100

    AddedHelperVariables
    Added helper variables to expenseItem component so that i can manipulate how they will appear before returning them in the JSX

--- src/App.js
+++ src/App.js
@@ -31,10 +31,10 @@ function App() {
   return (
     <div>
       <h2>Let's get started! Here we go !!! Changed again fammm!! Changed on Windows again!! One more time for windows</h2>
-      <ExpenseItem id={expenses[0].id} title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date.toISOString()}></ExpenseItem>
-      <ExpenseItem id={expenses[1].id} title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date.toISOString()}></ExpenseItem>
-      <ExpenseItem id={expenses[2].id} title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date.toISOString()}></ExpenseItem>
-      <ExpenseItem id={expenses[3].id} title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date.toISOString()}></ExpenseItem>
+      <ExpenseItem id={expenses[0].id} title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}></ExpenseItem>
+      <ExpenseItem id={expenses[1].id} title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}></ExpenseItem>
+      <ExpenseItem id={expenses[2].id} title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}></ExpenseItem>
+      <ExpenseItem id={expenses[3].id} title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date}></ExpenseItem>
     </div>
   );
 }
