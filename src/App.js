9ba280f8a01ef4186fe0b256dea397e8843cb7c1
f-regal <faysalregal@gmail.com>
2021-08-26 20:18:19 +0100

    SplitComponents
    Split the expenseItem component so that the date could be its own separate re-usable component

--- src/App.js
+++ src/App.js
@@ -30,7 +30,6 @@ function App() {
 
   return (
     <div>
-      <h2>Let's get started! Here we go !!! Changed again fammm!! Changed on Windows again!! One more time for windows</h2>
       <ExpenseItem id={expenses[0].id} title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}></ExpenseItem>
       <ExpenseItem id={expenses[1].id} title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}></ExpenseItem>
       <ExpenseItem id={expenses[2].id} title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}></ExpenseItem>
