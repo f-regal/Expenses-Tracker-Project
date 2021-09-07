5451e3ca396745ff3a2f7fd748bdf86be8d7c481
f-regal <faysalregal@gmail.com>
2021-09-07 16:34:25 +0100

    stateAssignment

--- src/Components/Expenses/Expenses.js
+++ src/Components/Expenses/Expenses.js
@@ -1,14 +1,29 @@
+import React, {useState} from 'react';
 import ExpenseItem from "./ExpenseItem";
+import ExpenseFilter from "./ExpenseFilter";
 import Card from '../UI/Card';
 
+
 function Expenses(props) {
+
+    const [filteredYear, setInputYear] = useState('2020');
+
+    const saveYearHandler = (year) => {
+        setInputYear(year)
+    }
+
+    
+
     return (
-        <Card className = "expenses">
-            <ExpenseItem id={props.items[0].id} title={props.items[0].title} amount={props.items[0].amount} date={props.items[0].date}></ExpenseItem>
-            <ExpenseItem id={props.items[1].id} title={props.items[1].title} amount={props.items[1].amount} date={props.items[1].date}></ExpenseItem>
-            <ExpenseItem id={props.items[2].id} title={props.items[2].title} amount={props.items[2].amount} date={props.items[2].date}></ExpenseItem>
-            <ExpenseItem id={props.items[3].id} title={props.items[3].title} amount={props.items[3].amount} date={props.items[3].date}></ExpenseItem>
-        </Card>
+        <div>
+            <Card className = "expenses">
+                <ExpenseFilter selected={filteredYear} onSaveYear = {saveYearHandler}/>
+                <ExpenseItem id={props.items[0].id} title={props.items[0].title} amount={props.items[0].amount} date={props.items[0].date}></ExpenseItem>
+                <ExpenseItem id={props.items[1].id} title={props.items[1].title} amount={props.items[1].amount} date={props.items[1].date}></ExpenseItem>
+                <ExpenseItem id={props.items[2].id} title={props.items[2].title} amount={props.items[2].amount} date={props.items[2].date}></ExpenseItem>
+                <ExpenseItem id={props.items[3].id} title={props.items[3].title} amount={props.items[3].amount} date={props.items[3].date}></ExpenseItem>
+            </Card>
+        </div>
     )
 }
 
