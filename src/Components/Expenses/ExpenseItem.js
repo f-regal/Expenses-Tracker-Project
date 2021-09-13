eeb44f28ba70a5216cad25ea19c170754fcec34a
f-regal <faysalregal@gmail.com>
2021-09-13 19:39:06 +0100

    CanAddExpenseItemsUI

--- src/Components/Expenses/ExpenseItem.js
+++ src/Components/Expenses/ExpenseItem.js
@@ -1,16 +1,11 @@
-import {useState} from 'react'
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 import Card from '../UI/Card'
 
 function ExpenseItem(props) {
 
-    const [title, setTitle] = useState(props.title);
+   
     
-
-    const clickHandler = () => {
-        setTitle('updated!!!');
-    }
     
 
 
@@ -21,10 +16,10 @@ function ExpenseItem(props) {
            <ExpenseDate date1 = {props.date}></ExpenseDate>
 
             <div className="expense-item__description">
-                <h2>{title}</h2>
+                <h2>{props.title}</h2>
                 <div className="expense-item__price">{props.amount}</div>
             </div>
-            <button onClick = {clickHandler}>Change title</button>
+            
 
         </Card>
     )
