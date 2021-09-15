3ae67b888c9b910b060f60d8a8c874af17766ce1
Faysal Regal <faysalregal@gmail.com>
2021-09-15 20:14:15 +0100

    FilteringArraysAttempt

--- src/Components/Expenses/Expenses.js
+++ src/Components/Expenses/Expenses.js
@@ -7,23 +7,44 @@ import Card from '../UI/Card';
 function Expenses(props) {
 
     const [filteredYear, setInputYear] = useState('2020');
-
+    //const newList = [];
     const saveYearHandler = (year) => {
         setInputYear(year)
 
+        //FIRST TRY
+        // props.items.forEach(expense => {
+        //     if (year === (expense.date.getYear()+1900).toString()) {
+        //         //console.log(expense);
+        //         newList.push(expense);
+        //     }
+        // });
+        // console.log(newList);
+        // console.log(props.items)
+
+        //{newList.map(expense => <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date}/>)}
+        // {props.items.filter(expense => expense.date.includes('2021')).map(expense => (
+        //     <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date}/>))
+        // }
+
+
+
+        //SECOND TRY
+        // const list1 = [];
+        // const newExpenses = props.items.forEach(expense => {
+        //     list1.push(expense);
+        // });
+        // console.log(list1);
+
+        // const newerExpenses = list1.filter(expense => {return (expense.date)} );
+        // console.log(newerExpenses);
+        //{if (year === expense.date.getYear().toString()) {return console.log(expense)}}
+
+        //GREAT RESOURCE TO LEARN FILTERING OF ARRAYS IN REACT - https://sebhastian.com/react-filter/
+
+
+       
     }
 
-    //const listOfYears =  props.items.map(expense => expense.date.getYear()+1900);
-    //{props.items.map(expense => console.log(expense.date.getYear()+1900))}
-    //basically for each year in our list which is equal to the one selected, create expense item for it.
-
-    //    const getThatone = (x) => {
-    //     console.log(x);
-    //    }
-
-    //This supposed to go under the jsx;
-    //{console.log(getThatone(listOfYears))}
-  
 
     return (
         <div>
@@ -31,6 +52,8 @@ function Expenses(props) {
                 <ExpenseFilter selected={filteredYear} onSaveYear = {saveYearHandler}/>
 
                 {props.items.map(expense => <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date}/>)}
+                
+
                 
             </Card>
         </div>
