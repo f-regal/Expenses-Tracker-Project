90447a24360f40da5c0929df417480dde6502347
f-regal <faysalregal@gmail.com>
2021-09-27 21:31:09 +0100

    RenderingFilteredList

--- src/Components/Expenses/Expenses.js
+++ src/Components/Expenses/Expenses.js
@@ -9,52 +9,38 @@ function Expenses(props) {
     const [filteredYear, setInputYear] = useState('2020');
     //const newList = [];
     const saveYearHandler = (year) => {
-        setInputYear(year)
-
-        //FIRST TRY
-        // props.items.forEach(expense => {
-        //     if (year === (expense.date.getYear()+1900).toString()) {
-        //         //console.log(expense);
-        //         newList.push(expense);
-        //     }
-        // });
-        // console.log(newList);
-        // console.log(props.items)
-
-        //{newList.map(expense => <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date}/>)}
-        // {props.items.filter(expense => expense.date.includes('2021')).map(expense => (
-        //     <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date}/>))
-        // }
-
-
-
-        //SECOND TRY
-        // const list1 = [];
-        // const newExpenses = props.items.forEach(expense => {
-        //     list1.push(expense);
-        // });
-        // console.log(list1);
-
-        // const newerExpenses = list1.filter(expense => {return (expense.date)} );
-        // console.log(newerExpenses);
-        //{if (year === expense.date.getYear().toString()) {return console.log(expense)}}
-
+        setInputYear(year);
+        //const Year1 = year;
+        //console.log(Year1);
+        return year;
+        
         //GREAT RESOURCE TO LEARN FILTERING OF ARRAYS IN REACT - https://sebhastian.com/react-filter/
+        //let expenses = props.items;
+        //console.log(expenses)
 
+        // expenses.forEach(expense => {
+        //     if (year === (expense.date.getYear()+1900).toString()) {
+        //         console.log(expense);
+        //     }
+            
+        // });
 
-       
+        //SO CLOSE
+        //expenses.filter(expense => (expense.date.getYear()+1900).toString() === year).map(thisYearExpense => console.log(thisYearExpense))
+
+        //Put this in JSX
+        //{props.items.filter(expense => (expense.date.getYear()+1900).toString() === saveYearHandler(year)).map(thisExpense => <ExpenseItem key={thisExpense.id} title={thisExpense.title} amount={thisExpense.amount} date={thisExpense.date}/>)} 
+
+        //Actual JSX
+        //{props.items.map(expense => <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date}/>)}
     }
 
 
     return (
         <div>
             <Card className = "expenses">
-                <ExpenseFilter selected={filteredYear} onSaveYear = {saveYearHandler}/>
-
-                {props.items.map(expense => <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date}/>)}
-                
-
-                
+                <ExpenseFilter selected={filteredYear} onSaveYear = {saveYearHandler}/>          
+                {props.items.map(expense => <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date}/>)} 
             </Card>
         </div>
     )
